1d127843dcea1b1dd7c5366fc3f7aa1ce2f66a3d
justice30 <justice_jl@163.com>
2019-05-03 17:44:36 +0800

    beego OK

--- eventlog.go
+++ eventlog.go
@@ -1,9 +1,9 @@
 package main
 import (
 	"fmt"
-	"github.com/elastic/beats/libbeat/common"
-	//"golang.org/x/sys/windows/svc/eventlog"
-	"github.com/elastic/beats/winlogbeat/eventlog"
+	_ "github.com/elastic/beats/libbeat/common"
+	_ "github.com/elastic/beats/winlogbeat/eventlog"
+	"golang.org/x/sys/windows/svc/eventlog"
 	"os"
 )
 
@@ -13,23 +13,23 @@ func main() {
 	fmt.Println("爱金金")
 	s :=os.Environ()
 	fmt.Println(s)
-	var option common.Config
-	var elog, err = eventlog.New((*common.Config)(option))
-	if err == nil{
-		fmt.Println("eventlog异常")
-		return
-	}
-	var name = elog.Name();
-	fmt.Println(name)
-	//var elog, err = eventlog.Open("jinlong")
-	//if err != nil {
+	//var option common.Config
+	//var elog, err = eventlog.New((*common.Config)(option))
+	//if err == nil{
+	//	fmt.Println("eventlog异常")
 	//	return
 	//}
-	//defer elog.Close()
-	//print()
-	//elog.Info(1, "金龙测试:爱金金！ 应用程序日志： 信息")
-	//elog.Warning(2, "金龙测试:爱金金！ 应用程序日志： 警告")
-	//elog.Error(3, "金龙测试:爱金金！ 应用程序日志： 错误")
-	//println(err)
+	//var name = elog.Name();
+	//fmt.Println(name)
+	var elog, err = eventlog.Open("jinlong")
+	if err != nil {
+		return
+	}
+	defer elog.Close()
+	print()
+	elog.Info(1, "金龙测试:爱金金！ 应用程序日志： 信息")
+	elog.Warning(2, "金龙测试:爱金金！ 应用程序日志： 警告")
+	elog.Error(3, "金龙测试:爱金金！ 应用程序日志： 错误")
+	println(err)
 
 }
