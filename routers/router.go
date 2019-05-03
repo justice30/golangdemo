de778242ad8101f47fdcba21a2192f7783939cb5
justice30 <justice_jl@163.com>
2019-05-03 19:08:24 +0800

    beego OK

--- routers/router.go
+++ routers/router.go
@@ -1,10 +1,11 @@
 package routers
 
 import (
-	"hello/controllers"
 	"github.com/astaxie/beego"
+	"golangdemo/controllers"
 )
 
 func init() {
     beego.Router("/", &controllers.MainController{})
+    beego.Router("/users",&controllers.UserController{})
 }
