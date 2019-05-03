d4d117d66320397206a4d234b380621929e17087
justice30 <justice_jl@163.com>
2019-05-03 16:43:28 +0800

    beego OK

--- eventlog.go
+++ eventlog.go
@@ -1,20 +1,35 @@
 package main
 import (
 	"fmt"
-	"golang.org/x/sys/windows/svc/eventlog"
+	"github.com/elastic/beats/libbeat/common"
+	//"golang.org/x/sys/windows/svc/eventlog"
+	"github.com/elastic/beats/winlogbeat/eventlog"
+	"os"
 )
+
+
 
 func main() {
 	fmt.Println("爱金金")
-	var elog, err = eventlog.Open("jinlong")
-	if err != nil {
+	s :=os.Environ()
+	fmt.Println(s)
+	var option common.Config
+	var elog, err = eventlog.New((*common.Config)(option))
+	if err == nil{
+		fmt.Println("eventlog异常")
 		return
 	}
-	defer elog.Close()
-	print()
-	elog.Info(1, "金龙测试:爱金金！ 应用程序日志： 信息")
-	elog.Warning(2, "金龙测试:爱金金！ 应用程序日志： 警告")
-	elog.Error(3, "金龙测试:爱金金！ 应用程序日志： 错误")
-	println(err)
+	var name = elog.Name();
+	fmt.Println(name)
+	//var elog, err = eventlog.Open("jinlong")
+	//if err != nil {
+	//	return
+	//}
+	//defer elog.Close()
+	//print()
+	//elog.Info(1, "金龙测试:爱金金！ 应用程序日志： 信息")
+	//elog.Warning(2, "金龙测试:爱金金！ 应用程序日志： 警告")
+	//elog.Error(3, "金龙测试:爱金金！ 应用程序日志： 错误")
+	//println(err)
 
 }
--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	_ "golangdemo/routers"
 	"github.com/astaxie/beego"
 )
 
 func main() {
-	beego.Run()
+	beego.Run("127.0.0.1:8888")
 }
+
