19284a41c9f60a09be839b6d09990e6a631e582a
justice30 <justice_jl@163.com>
2019-05-04 17:39:16 +0800

    beego OK

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
-	_ "golangdemo/routers"
 	"github.com/astaxie/beego"
+	_ "golangdemo/routers"
 )
 
 func main() {
+	//设置静态目录，下载目录
+	beego.SetStaticPath("/down1", "download1")
+	beego.SetStaticPath("/down2", "download2")
+	//beego.AddTemplateExt("html")
 	beego.Run("127.0.0.1:8888")
+
 }
 
