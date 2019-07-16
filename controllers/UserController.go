a72025f7c300c66b0afd59b4ce9600d9bcc6aba1
justice30 <justice_jl@163.com>
2019-07-16 22:33:56 +0800

    beego OK

--- controllers/UserController.go
+++ controllers/UserController.go
@@ -1,11 +1,20 @@
 package controllers
 
-import "github.com/astaxie/beego"
+import (
+	"fmt"
+	"github.com/astaxie/beego"
+)
 
 type UserController struct {
 	beego.Controller
 }
 
-func (c *UserController) Get(){
+func (c *UserController) URLMapping() {
+	c.Mapping("users", c.Get)
+}
 
+// @router /users/:id [get]
+func (c *UserController) Get(){
+	id := c.GetString("id")
+	fmt.Println("=================id:"+id)
 }
--- routers/router.go
+++ routers/router.go
@@ -7,5 +7,8 @@ import (
 
 func init() {
     beego.Router("/", &controllers.MainController{})
-    beego.Router("/users",&controllers.UserController{})
+    //beego.Router("/users",&controllers.UserController{})
+	//beego.Router("/users/:id",&controllers.UserController{})
+	//注解路由
+	beego.Include(&controllers.UserController{})
 }
